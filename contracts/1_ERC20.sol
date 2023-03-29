596ba0256d874651d0dd8bbd89b5ac0f5a2f85cb
supriya anand <supriya.anand@coindcx.com>
2023-03-29 14:32:49 +0530

    minor updates

--- contracts/1_ERC20.sol
+++ contracts/1_ERC20.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+//This contract is for the ERC20 token which is used as a mode of investment in the Crowdfunding project
 contract CustomCrowdfundingToken is ERC20, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 1000000 * 10 ** decimals());
