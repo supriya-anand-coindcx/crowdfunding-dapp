149b5c9e8afafd19d01ff003e6e5bbcfbdceadd5
sachinkumawatcoindcx <sachin.kumawat@coindcx.com>
2023-03-29 07:11:45 +0530

    naming changes

--- ui/src/components/Layout.js
+++ ui/src/components/Layout.js
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { Welocme } from "./welocme";
+import { Welcome } from "./welcome";
 
 export const Layout = () => {
   return (
     <>
-    <Welocme />
+    <Welcome />
     <Navbar bg="dark" expand="lg">
       <Container>
         <Navbar.Brand href="/admin">Admin</Navbar.Brand>
--- ui/src/components/welcome.js
+++ ui/src/components/welcome.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Welocme = () => {
+export const Welcome = () => {
     console.log(process.env.REACT_APP_I);
     return (<h1>Welocme to Crowdfunding</h1>);
 }
